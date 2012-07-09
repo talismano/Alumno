04bcfe4de98be1551b55ed790a87069cd4562aaa
talismano <georgemorris@me.com>
2012-07-08 19:47:14 -0700

    add admin user creation

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
 class BootStrap {
+    def springSecurityService
+
 
     def init = { servletContext ->
+
+        def adminUser = SecUser.findByUsername('admin') ?: new SecUser(
+                username: 'admin',
+                password: 'lghsdirectory',
+                enabled: true).save(failOnError: true)
+
+        if (!adminUser.authorities.contains(adminRole)) {
+            SecUserSecRole.create adminUser, adminRole
+        }
+
+
     }
     def destroy = {
     }
